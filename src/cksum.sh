209c7f769c98534dd6cdf080e3fdd88aa43345a8
Bruno <bruno.sant.a@gmail.com>
2022-01-17 22:36:27 -0500

    feat: implementation checksum

--- src/cksum.sh
+++ src/cksum.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+# Script: cksum.sh
+# Author: Bruno de Santana | Kevin Clovel | Mario Camelo
+# Date: 17/01/2022
+# Description: 
+#
+#
+#
+#
+
 echo "---> Creating CKSUM file..."
+SHA_SERVEUR=`ssh $1@$2 "sha256sum $3"`
+SHA_CLIENT=`sha256sum $3`
+
+if [ SHA_SERVEUR = SHA_CLIENT ]
+then
+  echo "Transfert effectué avec succès"
+else
+  echo "Il y a peut-être eu une erreur dans le transfert"
+fi
