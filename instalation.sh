227d46fa30d47c034ba3d9e4be1038d1ddf55f24
Bruno <bruno.sant.a@gmail.com>
2022-01-19 00:51:16 -0500

    test: install

--- instalation.sh
+++ instalation.sh
@@ -5,8 +5,23 @@
 #
 
 # Instalation des packages
-sudo apt-get install -y git openssh-server 
+#sudo apt-get install -y git openssh-server 
 
 # Créez l'utilisateur projet
 
-# ...
+# Chaque machine
+case $HOSTNAME in
+  "serveur")
+
+
+  ;;
+  "client")
+
+  ;;
+  "matrix1-serveur")
+    echo "vai"
+  ;;
+  *)
+    echo "non configuré"
+    ;;
+esac
